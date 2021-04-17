9e39ea012e7e003c3101fcb83cb7be37d8a9512c
bigmac <bigmac@MacBook-MacBook-Pro-de-bigmac.local>
2021-04-17 12:24:21 +0000

    adding

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,20 @@ import "./App.css";
 import Home from "./pages/Home";
 import { addBooks } from "./actions/bookActions";
 import { useDispatch } from "react-redux";
+import { getAllBooks } from "./services/bookService";
 
 const App = () => {
   const dispatch = useDispatch();
+
   useEffect(() => {
-    dispatch(addBooks());
+    async function fetchMyAPI() {
+      const data =  await getAllBooks();
+       dispatch(addBooks(data));
+    }
+
+    fetchMyAPI();
   }, []);
+
   return <Home />;
 };
 
