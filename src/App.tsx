88f26ad5015055932b4d32c19c4be65bb067bcce
TM-Consulting <mounaim.touinssi@gmail.com>
2021-04-19 19:10:52 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { addBooks } from "./actions/bookActions";
 import { useDispatch } from "react-redux";
+import Details from "./pages/Details";
 
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(addBooks());
   }, []);
-  return <Home />;
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/details/:id" exact>
+            <Details />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
