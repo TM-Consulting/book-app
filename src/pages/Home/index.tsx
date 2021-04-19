6c32e5b07ad50df81b6bc42f27d8ccdcce047998
TM-Consulting <mounaim.touinssi@gmail.com>
2021-04-19 20:09:14 +0200

    add react router dom

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,23 +7,30 @@ import { createStructuredSelector } from "reselect";
 import { makeSelectBooks } from "../../selectors/bookSelector";
 import { useSelector } from "react-redux";
 import { bookData } from "../../types";
+import { useHistory } from "react-router-dom";
 const booksState = createStructuredSelector({
   books: makeSelectBooks(),
 });
 const Home = () => {
+  const history = useHistory();
   const { books } = useSelector(booksState);
+  const myClick = (e: number) => {
+    history.push(`details/${e}`)
 
+  };
   return (
     <Container>
       <div className="App">
         {console.log("testttt", books)}
         <CustomForm />
         <div className="border_Box">
-          {books.map((item:bookData) => (
+          {books.map((item: bookData) => (
             <CustomCard
               title={item.title}
               description={item.description}
-              handleClick={() => {}}
+              handleClick={(e) => {
+                myClick(item.id);
+              }}
             />
           ))}
         </div>
--- src/services/bookService.ts
+++ src/services/bookService.ts
@@ -4,11 +4,25 @@ import { bookData } from "../types";
 const backEndUrl = process.env.REACT_APP_BACKEND_URL;
 
 export const getAllBooks = async () => {
-  var mydata:bookData[] = [];
+  var mydata: bookData[] = [];
   await axios.get(`${backEndUrl}/api/books`).then((res) => {
-    mydata=res.data;
+    mydata = res.data;
     return mydata;
+  });
+  return mydata;
+};
 
+export const getBook = async (id: string) => {
+  var mydata: bookData = {
+    title: "",
+    description: "",
+    id: 0,
+    created_at: "",
+    updated_at: "",
+  };
+  await axios.get(`${backEndUrl}/api/books/${id}`).then((res) => {
+    mydata = res.data;
+    return mydata;
   });
   return mydata;
 };
