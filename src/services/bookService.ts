38b8ae0a31be68f7cf4a042352ab45d518b5fc3e
TM-Consulting <mounaim.touinssi@gmail.com>
2021-04-22 19:49:09 +0200

    remove header

--- src/services/bookService.ts
+++ src/services/bookService.ts
@@ -2,19 +2,13 @@ import axios from "axios";
 import { bookData, bookDataSent } from "../types";
 
 const backEndUrl = process.env.REACT_APP_BACKEND_URL;
-const headers = {
-  "Access-Control-Allow-Origin": "http://localhost:3000",
-  "Access-Control-Allow-Headers": "*",
-  "Access-Control-Allow-Credentials": true,
-};
+
 export const getAllBooks = async () => {
   var mydata: bookData[] = [];
-  await axios
-    .get(`${backEndUrl}/api/books`, { headers: headers })
-    .then((res) => {
-      mydata = res.data;
-      return mydata;
-    });
+  await axios.get(`${backEndUrl}/api/books`).then((res) => {
+    mydata = res.data;
+    return mydata;
+  });
   return mydata;
 };
 
@@ -26,35 +20,29 @@ export const getBook = async (id: string) => {
     created_at: "",
     updated_at: "",
   };
-  await axios
-    .get(`${backEndUrl}/api/books/${id}`, { headers: headers })
-    .then((res) => {
-      mydata = res.data;
-      return mydata;
-    });
+  await axios.get(`${backEndUrl}/api/books/${id}`).then((res) => {
+    mydata = res.data;
+    return mydata;
+  });
   return mydata;
 };
 
 export const addBook = async (data: bookDataSent) => {
-  await axios
-    .post(`${backEndUrl}/api/books/`, data, { headers: headers })
-    .then((res) => {});
+  await axios.post(`${backEndUrl}/api/books/`, data).then((res) => {});
 };
 
 export const removeBook = async (id: string) => {
-  await axios.delete(`${backEndUrl}/api/books/${id}`, { headers: headers });
+  await axios.delete(`${backEndUrl}/api/books/${id}`);
 };
 
 export const updateBook = async (id: string, data: bookDataSent) => {
-  await axios.put(`${backEndUrl}/api/books/${id}`, data, { headers: headers });
+  await axios.put(`${backEndUrl}/api/books/${id}`, data);
 };
 export const searchBook = async (id: string) => {
   var mydata: bookData[] = [];
-  await axios
-    .get(`${backEndUrl}/api/books/search/${id}`, { headers: headers })
-    .then((res) => {
-      mydata = res.data;
-      return mydata;
-    });
+  await axios.get(`${backEndUrl}/api/books/search/${id}`).then((res) => {
+    mydata = res.data;
+    return mydata;
+  });
   return mydata;
 };
